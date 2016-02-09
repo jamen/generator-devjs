1f76b764d1baee9246ea3cd719d08f755ecf6bee
Jamen Marzonie <jamenmarz@gmail.com>
2016-02-08 18:49:28 -0700

    Add sample prompt

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,18 @@
 import { Base } from 'yeoman-generator';
 
 class DevJS extends Base {
-  init() {}
+  init() {
+    var done = this.async();
+    this.prompt({
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default: this.appname // Default to current folder name
+    }, function (answers) {
+      this.log(answers.name);
+      done();
+    }.bind(this));
+  }
   install() {}
 }
 
