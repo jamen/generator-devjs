77d757276624581985b11b0363b344d8c85a219b
Jamen Marzonie <jamenmarz@gmail.com>
2016-02-27 20:10:35 -0700

    Fix gulpfile.babel.js

--- src/_template/gulpfile.babel.js
+++ src/_template/gulpfile.babel.js
@@ -1,18 +1,19 @@
 import gulp from 'gulp';
 const master = [];
-function task(...args) { gulp.task(...args); master.push(args[0]) };
+const task = gulp.task;
+gulp.task = function(...a) { master.push(a[0]); return task(...a) };
 <% if (typeof gulp_babel !== 'undefined' && gulp_babel) { %>
 import babel from 'gulp-babel';
 import del from 'del';
-task('clean:javascript', () => del(['out/**.js']));
-task('build:javascript', () =>
+gulp.task('clean:javascript', () => del(['out/**.js']));
+gulp.task('build:javascript', () =>
   gulp.src('src/**.js', { base: 'src' })
     .pipe(babel())
     .pipe(gulp.dest('lib'))
 );
 <% } if (typeof gulp_eslint !== 'undefined' && gulp_eslint) { %>
 import eslint from 'gulp-eslint';
-task('lint:javascript', () =>
+gulp.task('lint:javascript', () =>
   gulp.src('src/**.js')
     .pipe(eslint())
     .pipe(eslint.format())
